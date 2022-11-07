7799bfeaacc3342c5395ee8e280e499f13eaf16b
Patrick <4031292+patmood@users.noreply.github.com>
2022-11-07 10:46:44 -0800

    add v0.8 schema

--- test/pocketbase-types-example.ts
+++ test/pocketbase-types-example.ts
@@ -16,46 +16,63 @@ export type BaseRecord = {
 }
 
 export enum Collections {
-	EveryType = "every_type",
-	Orders = "orders",
-	Profiles = "profiles",
+	Base = "base",
+	CustomAuth = "custom_auth",
+	Everything = "everything",
+	Posts = "posts",
+	Users = "users",
 }
 
-export type EveryTypeRecord<Tjson_field = unknown> = {
-	text_field: string
-	number_field: number
-	bool_field: boolean
+export type BaseRecord = {
+	field?: string
+}
+
+export type BaseResponse = BaseRecord & BaseRecord
+
+export type CustomAuthRecord = {
+	custom_field?: string
+}
+
+export type CustomAuthResponse = CustomAuthRecord & BaseRecord
+
+export type EverythingRecord<Tanother_json_field = unknown, Tjson_field = unknown> = {
+	text_field?: string
+	number_field?: number
+	bool_field?: boolean
 	email_field?: string
-	url_field?: string
+	url_field: string
 	date_field?: IsoDateString
 	select_field?: "optionA" | "optionB" | "optionC"
 	json_field?: null | Tjson_field
+	another_json_field?: null | Tanother_json_field
 	file_field?: string
-	relation_field?: RecordIdString
-	user_field?: UserIdString
+	three_files_field?: string[]
+	user_relation_field?: RecordIdString
+	custom_relation_field?: RecordIdString
 }
 
-export type EveryTypeResponse<Tjson_field = unknown> = EveryTypeRecord<Tjson_field> & BaseRecord
+export type EverythingResponse<Tanother_json_field = unknown, Tjson_field = unknown> = EverythingRecord<Tanother_json_field, Tjson_field> & BaseRecord
 
-export type OrdersRecord = {
-	amount: number
-	payment_type: "credit card" | "paypal" | "crypto"
-	user: UserIdString
-	product: string
+export type PostsRecord = {
+	field?: string
+	nonempty_field: string
+	nonempty_bool: boolean
+	field1?: number
 }
 
-export type OrdersResponse = OrdersRecord & BaseRecord
+export type PostsResponse = PostsRecord & BaseRecord
 
-export type ProfilesRecord = {
-	userId: UserIdString
+export type UsersRecord = {
 	name?: string
 	avatar?: string
 }
 
-export type ProfilesResponse = ProfilesRecord & BaseRecord
+export type UsersResponse = UsersRecord & BaseRecord
 
 export type CollectionRecords = {
-	every_type: EveryTypeRecord
-	orders: OrdersRecord
-	profiles: ProfilesRecord
+	base: BaseRecord
+	custom_auth: CustomAuthRecord
+	everything: EverythingRecord
+	posts: PostsRecord
+	users: UsersRecord
 }
