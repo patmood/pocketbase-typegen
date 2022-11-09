83c6e3f1d27ae0af5e848a02926a126b43995dc9
Patrick Moody <4031292+patmood@users.noreply.github.com>
2022-11-09 14:01:22 -0800

    e2e integration test (#18)
    * add dockerfile to run e2e tests
    
    * add db typegen
    
    * cleanup
    
    * add test
    
    * add github workflow
    
    * remove interactive flag
    
    * intentionally fail integration test
    
    * save artifacts in case of failing tests
    
    * fix output dir

---
+++ test/integration/integration.js
@@ -0,0 +1,24 @@
+import assert from "node:assert"
+import fs from "fs/promises"
+
+// Known good types from repo
+const controlTypes = await fs.readFile("pocketbase-types-example.ts", {
+  encoding: "utf8",
+})
+
+async function testCreateFromUrl() {
+  const typesFromUrl = await fs.readFile("output/pocketbase-types-url.ts", {
+    encoding: "utf8",
+  })
+  assert.equal(typesFromUrl, controlTypes)
+}
+
+async function testCreateFromDb() {
+  const typesFromDb = await fs.readFile("output/pocketbase-types-db.ts", {
+    encoding: "utf8",
+  })
+  assert.equal(typesFromDb, controlTypes)
+}
+
+await testCreateFromUrl()
+await testCreateFromDb()
