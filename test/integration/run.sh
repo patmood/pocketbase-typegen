519b760a3a5c5586161f6c5363f7aa609d9289a6
Patrick <4031292+patmood@users.noreply.github.com>
2022-11-08 20:43:36 -0800

    add db typegen

--- test/integration/run.sh
+++ test/integration/run.sh
@@ -9,5 +9,7 @@ echo "Starting integration test."
 sleep 1
 
 node ./dist/index.js --url http://0.0.0.0:8090 --email test@test.com --password testpassword --out pocketbase-types-url.ts
+node ./dist/index.js --db pb_data/data.db --out pocketbase-types-db.ts
 
 cat pocketbase-types-url.ts
+cat pocketbase-types-db.ts
