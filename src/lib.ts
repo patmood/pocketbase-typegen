3e65c0802e74c238006d745dbd2ebf83aa8237ec
Patrick <4031292+patmood@users.noreply.github.com>
2023-02-17 20:12:52 -0800

    formatting

--- src/lib.ts
+++ src/lib.ts
@@ -22,11 +22,7 @@ export function generate(results: Array<CollectionRecord>): string {
   const responseTypes: Array<string> = [RESPONSE_TYPE_COMMENT]
 
   results
-    .sort((a, b) => {
-      if (a.name < b.name) return -1
-      if (a.name > b.name) return 1
-      return 0
-    })
+    .sort((a, b) => (a.name <= b.name ? -1 : 1))
     .forEach((row) => {
       if (row.name) collectionNames.push(row.name)
       if (row.schema) {
