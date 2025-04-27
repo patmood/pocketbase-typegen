8e5cef18518140eb2097ccfcc5ab490609b3bbfb
patmood <4031292+patmood@users.noreply.github.com>
2025-04-28 09:01:52 +1000

    fix output directory

--- test/integration/integration.js
+++ test/integration/integration.js
@@ -7,21 +7,21 @@ const controlTypes = await fs.readFile("../pocketbase-types-example.ts", {
 })
 
 async function testCreateFromUrl() {
-  const typesFromUrl = await fs.readFile("./pocketbase-types-url.ts", {
+  const typesFromUrl = await fs.readFile("./output/pocketbase-types-url.ts", {
     encoding: "utf8",
   })
   assert.equal(typesFromUrl, controlTypes)
 }
 
 async function testCreateFromEnv() {
-  const typesFromEnv = await fs.readFile("./pocketbase-types-env.ts", {
+  const typesFromEnv = await fs.readFile("./output/pocketbase-types-env.ts", {
     encoding: "utf8",
   })
   assert.equal(typesFromEnv, controlTypes)
 }
 
 async function testCreateFromDb() {
-  const typesFromDb = await fs.readFile("./pocketbase-types-db.ts", {
+  const typesFromDb = await fs.readFile("./output/pocketbase-types-db.ts", {
     encoding: "utf8",
   })
   assert.equal(typesFromDb, controlTypes)
--- test/integration/test.sh
+++ test/integration/test.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 echo "Starting integration test."
 cd /app/test/integration
+mkdir -p output
 
 # Start pocketbase server
 ./pocketbase serve --http=0.0.0.0:8090 &
@@ -8,9 +9,9 @@ cd /app/test/integration
 echo "Waiting for server to start."
 while ! nc -z localhost 8090 </dev/null; do sleep 1; done
 
-node ../../dist/index.js --url http://0.0.0.0:8090 --email test@test.com --password testpassword --out ./pocketbase-types-url.ts
-node ../../dist/index.js --db pb_data/data.db --out ./pocketbase-types-db.ts
-node ../../dist/index.js --env --out ./pocketbase-types-env.ts
+node ../../dist/index.js --url http://0.0.0.0:8090 --email test@test.com --password testpassword --out ./output/pocketbase-types-url.ts
+node ../../dist/index.js --db pb_data/data.db --out ./output/pocketbase-types-db.ts
+node ../../dist/index.js --env --out ./output/pocketbase-types-env.ts
 
 node integration.js
 exit_status=$?
